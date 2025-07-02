4a3a47d784f302c3b013bc2bbdef82e9c38c750a
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-07-02 13:48:42 -0300

    Estrutura inicial

---
+++ src/main/java/com/centralconsig/dados/cliente/CentralConsigBancosDadosClienteApplication.java
@@ -0,0 +1,13 @@
+package com.centralconsig.dados.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConsigBancosDadosClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConsigBancosDadosClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centralconsig/dados/cliente/CentralConsigBancosDadosClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centralconsig.dados.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConsigBancosDadosClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
